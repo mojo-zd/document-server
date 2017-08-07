faa1fc0f994d2a4ac1c91b25339e6eddf4815220
mojo <excessivespeed@126.com>
2017-08-07 21:51:16 +0800

    generate diff excel

---
+++ document-server/redis/redis_client.go
@@ -0,0 +1,11 @@
+package redis
+
+import "github.com/go-redis/redis"
+
+func NewRedisClient() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+}
