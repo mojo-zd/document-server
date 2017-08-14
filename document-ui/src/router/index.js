ef9222ece949676d0995a091d6c6eac9ab392d77
mojo <excessivespeed@126.com>
2017-08-14 22:30:16 +0800

    document ui

--- document-ui/src/router/index.js
+++ document-ui/src/router/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import ElementUI from 'element-ui'
+import VueResource from 'vue-resource'
 import 'element-ui/lib/theme-default/index.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import '../assets/main.css'
 
 Vue.use(Router)
 Vue.use(ElementUI)
+Vue.use(VueResource)
 
 export default new Router({
   routes: [
